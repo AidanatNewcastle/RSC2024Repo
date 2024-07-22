8adca7af7898a201cbe26052b481231f7ef01c94
AidanatNewcastle <a.j.mullins2@newcastle.ac.uk>
2024-07-22 17:38:16 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,14 +81,16 @@ def makeplot(id,mfs,lfs):
     lp = lfs[id]
     lspa = lp[0]*(xspa**2) + lp[1]*xspa + lp[2]
 
+    xspa = xspa/l
+
     fig, ax = plt.subplots()
 
-    ax.scatter(tsam,xsam, color = (0.0,0.0,0.0), label = 'Generated Data')
+    ax.scatter(tsam,xsam, s = [0.0001]*len(tsam), color = (0.0,0.0,0.0), label = 'Generated Data')
     ax.plot(xspa,yspa, color = (0.0,0.0,0.0), label = 'True Trend')
     ax.plot(xspa,mspa, color = (0.0,0.247058823529412,0.447058823529412), label = 'MMD-Trained Quadratic')
-    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((mspa+mp[3]),(mspa-mp[3])), axis=None), c = np.array([[0.0,0.247058823529412,0.447058823529412]]), label = 'MMD-Trained Error')
+    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((mspa+mp[3]),(mspa-mp[3])), axis=None), s = [0.0001]*2000, c = np.array([[0.0,0.247058823529412,0.447058823529412]]), label = 'MMD-Trained Error')
     ax.plot(xspa,lspa, color = (0.980392156862745,0.976470588235294,0.964705882352941), label = 'LSQ-Trained Quadratic')
-    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((lspa+lp[3]),(lspa-lp[3])), axis=None), c = np.array([[0.980392156862745,0.976470588235294,0.964705882352941]]), label = 'LSQ-Trained Error')
+    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((lspa+lp[3]),(lspa-lp[3])), axis=None), s = [0.0001]*2000, c = np.array([[0.980392156862745,0.976470588235294,0.964705882352941]]), label = 'LSQ-Trained Error')
     ax.set_xlabel('t/L') 
     ax.set_ylabel('x') 
     ax.legend()
@@ -115,6 +117,6 @@ MLS, LLS, MFS, LFS = preparetestlevelsandfits()
 cLs = preparecLspace()
 cLselector = st.slider(label = 'Slide a test number to get a better look at the test! Drawing the slider up increases the cL tested.',min_value=0,max_value=399,value = 20,step=1)
 st.write('Power Test No.'+ str(cLselector) + ', cL = ', str(cLs[cLselector]))
-st.write('Proposed Test Level = ' + str(MLS[cLselector]))
-st.write('MEP-CvM Test Level = ' + str(LLS[cLselector]))
+st.write('Proposed Test Level = ' + str(int(MLS[cLselector])))
+st.write('MEP-CvM Test Level = ' + str(int(LLS[cLselector])))
 st.pyplot(makeplot(cLselector,MFS,LFS))
