e8123be19e8db3f408365ecd83f0a176f9d71287
AidanatNewcastle <a.j.mullins2@newcastle.ac.uk>
2024-07-22 17:35:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,105 @@
 import streamlit as st
 import pandas as pd
 import math
+import jax.numpy as np
+from jax import jit
+import h5py as h5
+import matplotlib.pyplot as plt
 from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Robust Kernelised Composite Goodness-of Fit Testing for Conditional Relationships Supplemental Web App',
+    page_icon=':iphone:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
+def quickget(ds,h5f):
+    return np.array(h5f.get(ds))
+
+@jit
 @st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def preparecLspace():
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    i = 0;
+    cLspace = [None]*400
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    stribuff = str(i)
+    Abuffer = 'PowerTestData' + stribuff + '.h5'
+    with A as h5.File(Abuffer,'r'):
+        cL = quickget('cL',A)
+        A.close()
+    cLspace[i] = cL
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    for i in range(1,400):
+            stribuff = str(i)
+        Abuffer = 'PowerTestData' + stribuff + '.h5'
+        with A as h5.File(Abuffer,'r'):
+            cL = quickget('cL',A)
+            A.close()
+        cLspace[i] = cL
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+    return np.array(cLspace).flatten()
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+@jit
+@st.cache_data
+def preparetestlevelsandfits():
 
-    return gdp_df
+    Bbuffer = 'PowerTestFits.h5'
+    with B as h5.File(Bbuffer,'r')
+        mmdlevelspace = quickget('MMDLevel',B)
+        lsqlevelspace = quickget('LSQLevel',B)
+        mmdfitspace = quickget('MMDFit',B)
+        lsqfitspace = quickget('LSQFit',B)
+        B.close()
 
-gdp_df = get_gdp_data()
+    return mmdlevelspace, lsqlevelspace, mmdfitspace, lsqfitspace
+
+@jit
+@st.cache_data
+def prepareplotdata(rs):
+
+    Cbuffer = 'PowerTestData' + rs + '.h5'
+
+    with C as h5.File(Cbuffer,'r'):
+        a = quickget('a',C)
+        b = quickget('b',C)
+        L = quickget('L',C)
+        cL = quickget('cL',C)
+        tsample = quickget('TSample',C)
+        xsample = quickget('XSample',C) 
+        C.close()
+    
+    yspace = a + b*np.exp(np.linspace(-cL,cL,1000))
+
+    return tsample,xsample,yspace,L
+
+def makeplot(id,mfs,lfs):
+    
+    ids = str(id)
+    tsam,xsam,yspa,l = prepareplotdata(ids)
+    xspa = np.linspace(-l,l,1000)
+
+    mp = mfs[:,id]
+    mspa = mp[0]*(xspa**2) + mp[1]*xspa + mp[2]
+    lp = lfs[:,id]
+    lspa = lp[0]*(xspa**2) + lp[1]*xspa + lp[2]
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(tsam,xsam, color = (0.0,0.0,0.0), label = 'Generated Data')
+    ax.plot(xspa,yspa, color = (0.0,0.0,0.0) label = 'True Trend')
+    ax.plot(xspa,mspa, color = (0.0,0.247058823529412,0.447058823529412), label = 'MMD-Trained Quadratic')
+    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((mspa+mp[3]),(mspa-mp[3])), axis=None), c = np.array([[0.0,0.247058823529412,0.447058823529412]]), label = 'MMD-Trained Error')
+    ax.plot(xspa,lspa, color = (0.980392156862745,0.976470588235294,0.964705882352941), label = 'LSQ-Trained Quadratic')
+    ax.scatter(np.concatenate((xspa,xspa), axis=None),np.concatenate(((lspa+lp[3]),(lspa-lp[3])), axis=None), c = np.array([[0.980392156862745,0.976470588235294,0.964705882352941]]), label = 'LSQ-Trained Error')
+    ax.xlabel('t/L') 
+    ax.ylabel('x') 
+    ax.legend()
+
+    return fig
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -75,77 +117,11 @@ But it's otherwise a great (and did I mention _free_?) source of data.
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+MLS, LLS, MFS, LFS = preparetestlevelsandfits()
+cLs = preparecLspace()
+cLselector = st.slider(label = 'Slide a test number to get a better look at the test! Drawing the slider up increases the cL tested.',min_value=0,max_value=399,value = 20,step=1)
+cLselected = cLs[cLsselector]
+st.write('Power Test No.'+ str(cLselector) + ', cL = ', str(cLs[cLselector]))
+st.write('Proposed Test Level = ' + str(MLS[cLselector]))
+st.write('MEP-CvM Test Level = ' + str(LLS[cLselector]))
+st.pyplot(makeplot(cLselector,MFS,LFS))
