8f7b6492d26576bb8a13fff60439f20d87097860
AidanatNewcastle <a.j.mullins2@newcastle.ac.uk>
2024-07-23 16:18:53 +0100

    update appsource.py

--- appsource.py
+++ appsource.py
@@ -171,7 +171,7 @@ with lcol:
     '''
     # :globe_with_meridians: Robust Kernelised Composite Goodness-of-Fit Testing for Conditional Relationships Supplemental Web App
 
-    Hello! This a Streamlit-powered web app to support a poster made for RSC Exeter 2024. This contextualises a figure profiling the power of a statistical test presented on the poster, namely by letting the reader see the data and the underlying behaviour thereof as well as the quadratic models fitted by the proposed and conventional methods and the corresponding Goodness-of-Fit confidences recorded. 
+    Hello! This a Streamlit-powered web app to support a poster made for RSC Exeter 2024. This contextualises a figure profiling the power of a statistical test presented on the poster, namely by letting the reader see the data plotted, point-by-point, next to the underlying test each plotted point represents. The principle image of the figure is included [WIP]. 
 
     Only keeping the app open if it's in use would be deeply appreciated, as the overall resources available to this app are limited.
     '''
